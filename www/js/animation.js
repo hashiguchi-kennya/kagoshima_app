d5525a05f37f5b1a64e4283a03d845ab2a9a2fe2
Hashiguchi Kenya <hashiguchi@griffin-net.co.jp>
2020-06-10 15:13:47 +0900

    change

--- www/js/animation.js
+++ www/js/animation.js
@@ -1,16 +1,2 @@
-'use strict';
+import anime from 'animejs';
 
-var body = document.getElementsByTagName('body')[0];
-
-function fadeOut() {
-    body.classList.add('bodyfadeout');
-}
-
-function linkUrl() {
-    location.href = 'index.html';
-}
-var bt1 = document.getElementById('start');
-bt1.addEventListener('click', function() {
-    fadeOut();
-    setTimeout(linkUrl, 1500);
-});
